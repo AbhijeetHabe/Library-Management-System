1637b450be03a574d21f563654c802cf8a7fb0ad
AbhijeetHabe <abhijeethabe23@gmail.com>
2023-06-17 23:54:52 +0530

    Changes updated

--- Library-Management2/src/com/cdac/dao/UserDao.java
+++ Library-Management2/src/com/cdac/dao/UserDao.java
@@ -1,14 +1,15 @@
 package com.cdac.dao;
 
 import java.util.List;
-
 import com.cdac.dto.Books;
 import com.cdac.dto.User;
 
 public interface UserDao {
-	
+
 	void insertUser(User user);
+
 	boolean checkUser(User user);
+
 	public List<Books> selectAll(Books books);
 
 }
